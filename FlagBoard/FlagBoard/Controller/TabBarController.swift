e7cad82fe5756234274add1d0dc71565ed0eaf89
shin seung jae <shinseungjae1234@gmail.com>
2022-10-07 11:56:06 +0900

    add: storyboard reference 참고하여 tab bar controller & navigation controller 추가

---
+++ FlagBoard/FlagBoard/Controller/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2022/10/07.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
