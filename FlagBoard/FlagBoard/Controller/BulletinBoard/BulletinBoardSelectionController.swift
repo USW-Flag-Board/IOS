994ebb6a7e6a7987f03b4abd745017ea11a7cb1e
moonazn <sjy3729@naver.com>
2022-10-12 01:38:00 +0900

    Feat: 게시판 선택 화면에 tableViewCell 추가
    게시판 선택 화면에 tableViewCell 게시판 목록을 추가함

--- FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardSelectionController.swift
+++ FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardSelectionController.swift
@@ -1,24 +1,60 @@
 import UIKit
 
-class BulletinBoardSelectionController: UIViewController {
+class BulletinBoardSelectionController: UIViewController{
 
+    
     //MARK: IBOutlets
+    @IBOutlet weak var boardSeletionTableview: UITableView!
+    
+    var boardNameList : [String] = ["!!공지!!", "정보게시판", "자유게시판", "동아리 이모저모", "Activity"]
     
     //MARK: Properties
     
     //MARK: Functions
+    private func registerXib(){
+        
+        //tableView에 cell 등록
+        let nibName = UINib(nibName: "BulletinBoardSelectionTableViewCell", bundle: nil)
+        
+        boardSeletionTableview.register(nibName, forCellReuseIdentifier: "boardSelectionTableCell")
+    }
+        
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        registerXib()
+        
+        boardSeletionTableview.delegate = self
+        boardSeletionTableview.dataSource = self
     }
     
-    //MARK: Button Actions
-    
-    //MARK: Design Functions
-    
 
- 
 
 }
+
+
+ // MARK: - UITableViewDataSource
+extension BulletinBoardSelectionController: UITableViewDelegate, UITableViewDataSource
+{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return boardNameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = boardSeletionTableview.dequeueReusableCell(withIdentifier: "boardSelectionTableCell", for: indexPath) as! BulletinBoardSelectionTableViewCell
+        
+        cell.boardLabel.text = boardNameList[indexPath.row]
+        
+        return cell
+    }
+
+}
+
+
+
+
