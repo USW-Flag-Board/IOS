d38195ae51d60248f9b72774dacf50947eb3cb71
shin seung jae <shinseungjae1234@gmail.com>
2022-10-11 21:24:03 +0900

    feat: 네비게이션 상단우측에 버튼 & function 추가

--- FlagBoard/FlagBoard/Controller/Profile/ProfileController.swift
+++ FlagBoard/FlagBoard/Controller/Profile/ProfileController.swift
@@ -1,8 +1,27 @@
-//
-//  ProfileController.swift
-//  FlagBoard
-//
-//  Created by 신승재 on 2022/10/07.
-//
+import UIKit
 
-import Foundation
+class ProfileController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    //MARK: Properties
+    
+    //MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Button Actions
+    
+    //MARK: Design Functions
+    
+    @IBAction func didTabMenuButton(_ sender: Any) {
+        
+    }
+    
+ 
+
+}
