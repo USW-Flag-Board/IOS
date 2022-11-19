eed57aa1fb8d647f311ba386a145532af7b3fb0f
shin seung jae <mok05247@naver.com>
2022-11-19 21:54:00 +0900

    feat: BulletinBoardDetailModel에 comment 더미 데이터 추가

--- FlagBoard/FlagBoard/Model/BulletinBoard/BulletinBoardDetailModel.swift
+++ FlagBoard/FlagBoard/Model/BulletinBoard/BulletinBoardDetailModel.swift
@@ -7,3 +7,27 @@
 
 import Foundation
 
+struct CommentData{
+
+    let contents: String
+    let author: String
+    let createAt: String
+    
+    init(contents: String, author: String, createAt: String){
+        self.contents = contents
+        self.author = author
+        self.createAt = createAt
+    }
+}
+
+#if DEBUG
+extension CommentData{
+    static var sampleData = [
+       CommentData(contents: "좋은 글 감사합니다 ^^", author: "정충일", createAt: "2022.08.04 22:11"),
+       CommentData(contents: "퍼가요 ^^", author: "박명범", createAt: "2022.08.04 23:33"),
+       CommentData(contents: "꿀팁이네요 ㅎㅎ", author: "강지은", createAt: "2022.08.04 23:53"),
+    ]
+    
+}
+
+#endif
