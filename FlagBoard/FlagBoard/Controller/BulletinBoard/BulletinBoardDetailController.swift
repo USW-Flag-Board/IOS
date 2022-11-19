99b9f80885f54390c056abd102bda9294156f6fa
shin seung jae <mok05247@naver.com>
2022-11-19 13:08:30 +0900

    feat: BulletinBoardDetailView에 필요한 label들 추가, Controller에 연결

--- FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardDetailController.swift
+++ FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardDetailController.swift
@@ -10,6 +10,12 @@ import UIKit
 class BulletinBoardDetailController: UIViewController {
 
     //MARK: IBOutlets
+    @IBOutlet weak var boardNameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var contentsLabel: UILabel!
+    @IBOutlet weak var createdAtLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
