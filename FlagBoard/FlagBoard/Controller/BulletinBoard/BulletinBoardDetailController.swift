7f612985f7d0efa598f3407ed0e70111b24b0239
shin seung jae <mok05247@naver.com>
2022-11-19 12:36:15 +0900

    add: BulletinBoardDetail swift & storyboard 추가

---
+++ FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardDetailController.swift
@@ -0,0 +1,22 @@
+//
+//  BulletinBoardDetailController.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2022/11/19.
+//
+
+import UIKit
+
+class BulletinBoardDetailController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+}
