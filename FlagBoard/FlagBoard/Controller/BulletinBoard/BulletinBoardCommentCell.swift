c58efdec5e2e61666b9e8dc51ea26dcd81383fa1
shin seung jae <mok05247@naver.com>
2022-11-19 22:11:58 +0900

    add: CommentCell & xib 파일 추가

---
+++ FlagBoard/FlagBoard/Controller/BulletinBoard/BulletinBoardCommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  BulletinBoardCommentCell.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2022/11/19.
+//
+
+import UIKit
+
+class BulletinBoardCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
