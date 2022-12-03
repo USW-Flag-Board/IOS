1b8a6e4e239bcb87f0643e6c5c0ec412c4fe0e9b
shin seung jae <mok05247@naver.com>
2022-12-03 11:04:19 +0900

    feat: 사이드메뉴 로그인 버튼 -> 로그인화면 연결

--- FlagBoard/FlagBoard/Controller/SideMenu/SideMenuController.swift
+++ FlagBoard/FlagBoard/Controller/SideMenu/SideMenuController.swift
@@ -3,8 +3,19 @@ import SideMenu
 
 class SideMenuController: UIViewController {
     
+    //MARK: IBOutlets
+    
+    //MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    //MARK: Functions
+    @IBAction func didTabLoginButton(_ sender: Any) {
+        let loginStoryboard = UIStoryboard(name: "LoginView", bundle: nil)
+        guard let loginViewController = loginStoryboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginController else { return }
+        
+        self.navigationController?.pushViewController(loginViewController, animated: true)
+    }
 }
