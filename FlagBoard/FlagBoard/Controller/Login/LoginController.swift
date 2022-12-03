b1228d274ee3452bc8614f58fdc224daf1368479
shin seung jae <mok05247@naver.com>
2022-12-03 10:53:14 +0900

    add: LoginController, View, Model 추가

---
+++ FlagBoard/FlagBoard/Controller/Login/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2022/12/03.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: IBOutlets
+    
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Functions
+
+}
---
+++ FlagBoard/FlagBoard/Model/Login/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2022/12/03.
+//
+
+import Foundation
