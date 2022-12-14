b93d2cb2f51e831ce26806bf75760fd6f6398a48
shin seung jae <mok05247@naver.com>
2022-12-14 19:57:52 +0900

    feat: 로그인 화면 아이디 패스워드 형식 검사 추가

--- FlagBoard/FlagBoard/Controller/Login/LoginController.swift
+++ FlagBoard/FlagBoard/Controller/Login/LoginController.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginController: UIViewController {
 
     //MARK: IBOutlets
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     //MARK: Properties
     
@@ -25,6 +27,25 @@ class LoginController: UIViewController {
         guard let registerViewController = registerStoryboard.instantiateViewController(withIdentifier: "RegisterVC") as? RegisterController else { return }
         
         self.navigationController?.pushViewController(registerViewController, animated: true)
+    }
+    
+    
+    // id 형식 검사(형식은 미정)
+    func isValidId(id: String?) -> Bool {
+        let idRegEx = ""
+        
+        let userId = NSPredicate(format:"SELF MATCHES %@", idRegEx)
+                  return userId.evaluate(with: id)
+    }
+    
+    // 비밀 번호 자릿수 검사
+    func isValidPassword(password: String?) -> Bool {
+        if let userPassword = password {
+                    if userPassword.count < 8 {
+                        return false
+                    }
+                }
+        return true
     }
     
 }
