36f5f8addf04815a5630a0e60d8d0745b1598308
shin seung jae <mok05247@naver.com>
2023-01-19 14:51:31 +0900

    add: RegisterIdAndPasswordController & RegisterIdAndPasswordView 추가

--- FlagBoard/FlagBoard/Controller/Login/RegisterIdAndPasswordController.swift
+++ FlagBoard/FlagBoard/Controller/Login/RegisterIdAndPasswordController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RegisterController: UIViewController {
+class RegisterIdAndPasswordController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
