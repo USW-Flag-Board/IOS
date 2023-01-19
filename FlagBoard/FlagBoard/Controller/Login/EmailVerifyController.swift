c052daa44c97aa262175831e63673f3537abe8e5
shin seung jae <mok05247@naver.com>
2023-01-19 15:17:03 +0900

    add: EmailVerifyController & View 추가

---
+++ FlagBoard/FlagBoard/Controller/Login/EmailVerifyController.swift
@@ -0,0 +1,29 @@
+//
+//  EmailVerifyController.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2023/01/19.
+//
+
+import UIKit
+
+class EmailVerifyController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
