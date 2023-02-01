62aa1b5f417073ac286ea421630b66c22febbadf
shin seung jae <mok05247@naver.com>
2023-02-01 17:24:12 +0900

    feat: UserRouter 형태만 잡아놓기

--- FlagBoard/FlagBoard/Network/UserRouter.swift
+++ FlagBoard/FlagBoard/Network/UserRouter.swift
@@ -1,8 +1,59 @@
 //
-//  UserRouter.swift
+//  RegisterRouter.swift
 //  FlagBoard
 //
 //  Created by 신승재 on 2023/02/01.
 //
 
 import Foundation
+
+import Alamofire
+
+// 현재 로그인한 사용자 정보 가져오기
+enum UserRouter: URLRequestConvertible {
+    
+    // 내 상세정보 보기(서버 미구현)
+    case getMyProfile
+    
+    var baseURL: URL {
+        return URL(string: AlamofireManger.BASE_URL)!
+    }
+
+
+    var path: String {
+        switch self {
+        case .getMyProfile:
+            return "/api/auth/check/id"
+        }
+    }
+
+
+    var method: HTTPMethod {
+        switch self {
+        case .getMyProfile:
+            return .get
+        }
+    }
+
+
+//        var parameters: Parameters {
+//            switch self {
+//            case let .getMyProfile:
+//                var params = Parameters()
+//                params["loginId"] = id
+//                return params
+//          }
+//        }
+        
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        
+        var request = URLRequest(url: url)
+        
+        request.method = method
+        request.httpBody = try JSONEncoding.default.encode(request).httpBody
+        
+        return request
+    }
+}
