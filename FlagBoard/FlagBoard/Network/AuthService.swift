3d2b8a175cbc76eeff11f93c6a12c4424fcf481c
shin seung jae <mok05247@naver.com>
2023-02-01 17:00:29 +0900

    add: 병합충돌 방지로 임시 코드 작성부분 추가, 키체인 매니저는 삭제

--- FlagBoard/FlagBoard/Network/AuthService.swift
+++ FlagBoard/FlagBoard/Network/AuthService.swift
@@ -9,9 +9,39 @@ import Foundation
 import Combine
 
 import Alamofire
+import KeychainSwift
 
-//enum AuthService {
+struct AuthService {
+    
+    let keyChain = KeychainSwift()
+    
+    func login(id: String, password: String) -> AnyPublisher<TokenData, AFError> {
+        print("AuthService - login() called")
+
+        return AlamofireManger.shared.session
+            .request(AuthRouter.login(id: id, password: password))
+            .publishDecodable(type: TokenData.self)
+            .value()
+            .map { receivedValue in
+                
+                keyChain.set(receivedValue.refreshToken, forKey: "refresh_token")
+                keyChain.set(receivedValue.accessToken, forKey: "access_token")
+                
+                return receivedValue
+            }.eraseToAnyPublisher()
+    }
+    
 //    static func reissueToken(accessToken: String, refreshToken: String) -> AnyPublisher<TokenData, AFError> {
 //        print("AuthService - reissueToken")
+//
+//        return AlamofireManger.shared.session
+//            .request(AuthRouter.reissueToken(accessToken: accessToken,
+//                                             refreshToken: refreshToken))
+//            .publishDecodable(type: TokenData.self)
+//            .value()
+//            .map { receivedValue in
+//                // 받은 키체인에서 처리
+//                return receivedValue
+//            }.eraseToAnyPublisher()
 //    }
-//}
+}
