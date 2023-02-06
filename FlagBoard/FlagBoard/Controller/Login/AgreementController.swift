817905eb55445c1af956bee8c4bb1cba9042522a
shin seung jae <mok05247@naver.com>
2023-02-06 16:21:16 +0900

    add: 약관 동의를 위한 agreement view & controller 추가

---
+++ FlagBoard/FlagBoard/Controller/Login/AgreementController.swift
@@ -0,0 +1,14 @@
+//
+//  AgreementController.swift
+//  FlagBoard
+//
+//  Created by 신승재 on 2023/02/06.
+//
+
+import UIKit
+
+import Alamofire
+
+class AgreementController: UIViewController {
+    
+}
