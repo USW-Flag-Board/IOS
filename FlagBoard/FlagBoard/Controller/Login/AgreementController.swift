c8f48f289ecb184b02b6dc50f61d89b520225602
shin seung jae <mok05247@naver.com>
2023-02-06 20:39:31 +0900

    feat: 약관 동의 로직 추가

--- FlagBoard/FlagBoard/Controller/Login/AgreementController.swift
+++ FlagBoard/FlagBoard/Controller/Login/AgreementController.swift
@@ -8,25 +8,85 @@
 import UIKit
 
 import Alamofire
+import DLRadioButton
 
 class AgreementController: UIViewController {
     
     //MARK: IBOutlets
     @IBOutlet weak var agreementButton: UIButton!
-    
+    @IBOutlet weak var allAgreeRadioButton: DLRadioButton!
+    @IBOutlet weak var firstRadioButton: DLRadioButton!
+    @IBOutlet weak var secondRadioButton: DLRadioButton!
     
     //MARK: Properties
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setButtonStyles()
+        setButtonOptions()
     }
     
     //MARK: @IBAction Functions
+    @IBAction func allAgreeRadioButtonPressed(_ sender: DLRadioButton) {
+        
+        if allAgreeRadioButton.isSelected {
+            changeRadioStatus(firstRadioButton, to: true)
+            changeRadioStatus(secondRadioButton, to: true)
+            changeButtonEnable(to: true)
+           
+        } else {
+            changeRadioStatus(firstRadioButton, to: false)
+            changeRadioStatus(secondRadioButton, to: false)
+            changeButtonEnable(to: false)
+        }
+    }
+    
+    
     
     //MARK: Functions
     func setButtonStyles() {
         self.agreementButton.layer.cornerRadius = 20
+    }
+    
+    func setButtonOptions() {
+        changeButtonEnable(to: false)
+    
+        allAgreeRadioButton.isMultipleSelectionEnabled = true
+        firstRadioButton.isMultipleSelectionEnabled = true
+        secondRadioButton.isMultipleSelectionEnabled = true
+        
+        firstRadioButton.addTarget(self, action: #selector(anyRadioButtonPressed), for: .touchUpInside)
+        secondRadioButton.addTarget(self, action: #selector(anyRadioButtonPressed), for: .touchUpInside)
+    }
+    
+    func changeRadioStatus(_ sender: DLRadioButton, to: Bool) {
+        if sender.isSelected != to {
+            sender.isSelected = to
+        }
+    }
+    
+    func changeButtonEnable(to: Bool) {
+        if to {
+            agreementButton.isEnabled = true
+            agreementButton.backgroundColor = #colorLiteral(red: 0.2156939507, green: 0.5384917259, blue: 0.4589682817, alpha: 1)
+        } else {
+            agreementButton.isEnabled = false
+            agreementButton.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.5568627715, blue: 0.5568627119, alpha: 1)
+        }
+    }
+    
+        
+    //MARK: @objc Functions
+    @objc func anyRadioButtonPressed() {
+        if firstRadioButton.isSelected || secondRadioButton.isSelected {
+            changeRadioStatus(allAgreeRadioButton, to: false)
+            changeButtonEnable(to: false)
+        }
+        if firstRadioButton.isSelected && secondRadioButton.isSelected {
+            changeRadioStatus(allAgreeRadioButton, to: true)
+            changeButtonEnable(to: true)
+        }
     }
 }
