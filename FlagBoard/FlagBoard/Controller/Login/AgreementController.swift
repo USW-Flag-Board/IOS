933562617ff70b29885e5a63e0ca8b7bfc3543dd
shin seung jae <mok05247@naver.com>
2023-02-06 17:43:49 +0900

    feat: agreement 화면 전체 배치

--- FlagBoard/FlagBoard/Controller/Login/AgreementController.swift
+++ FlagBoard/FlagBoard/Controller/Login/AgreementController.swift
@@ -11,4 +11,22 @@ import Alamofire
 
 class AgreementController: UIViewController {
     
+    //MARK: IBOutlets
+    @IBOutlet weak var agreementButton: UIButton!
+    
+    
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setButtonStyles()
+    }
+    
+    //MARK: @IBAction Functions
+    
+    //MARK: Functions
+    func setButtonStyles() {
+        self.agreementButton.layer.cornerRadius = 20
+    }
 }
