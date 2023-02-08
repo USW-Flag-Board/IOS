db440f22eed4f3bbc36dba3496198839fa7a0c7b
shin seung jae <mok05247@naver.com>
2023-02-08 20:49:49 +0900

    feat: 서버에서 id, email 중복확인시 받는 데이터 모델 추가

--- FlagBoard/FlagBoard/Model/Login/AuthModel.swift
+++ FlagBoard/FlagBoard/Model/Login/AuthModel.swift
@@ -9,7 +9,15 @@ import Foundation
 
 struct AuthModel {
     
-    // MARK: - Token Data
+    // MARK: - id and email overlab check data
+    struct OverlabCheckData: Codable {
+        let message: String
+        let payload: Bool
+        let status: String
+    }
+    
+    
+    // MARK: - token data
     struct TokenData: Codable {
         let accessToken: String
         let accessTokenExpiresIn: Int
