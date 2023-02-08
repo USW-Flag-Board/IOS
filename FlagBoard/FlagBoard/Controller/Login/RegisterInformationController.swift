b54dd800cb733a440109593119156ee5978b1257
shin seung jae <mok05247@naver.com>
2023-02-08 21:11:59 +0900

    feat: 텍스트필드, 레이블, 버튼 배치 및 다른화면도 통일성있게 수정

--- FlagBoard/FlagBoard/Controller/Login/RegisterInformationController.swift
+++ FlagBoard/FlagBoard/Controller/Login/RegisterInformationController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class RegisterInformationController: UIViewController {
     
+    //MARK: IBOutlets
+    
+    //MARK: Properties
     var id: String?
     var password: String?
     var joinType: String?
@@ -19,15 +22,13 @@ class RegisterInformationController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
+    // MARK: @IBAction Functions
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    // MARK: Functions
+    
+    
+    // @objc MARK: Functions
 }
